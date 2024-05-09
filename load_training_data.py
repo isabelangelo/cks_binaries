41c99c3449ec476e1338f745859b4e53f5a32cc7
Isabel Angelo <isabelangelo@astro.ucla.edu>
2024-05-09 13:31:50 -0700

    made it so training data for all orders saved to single .csv

--- load_training_data.py
+++ load_training_data.py
@@ -8,11 +8,12 @@ import pandas as pd
 import numpy as np
 import glob
 
+# ============ load literature data ==================================================
+
 # define paths to load and store spectrum files
 original_path = './data/cks-spectra/'
 shifted_resampled_path = './data/cks-spectra_shifted_resampled_r/'
-df_path = './data/cks-spectra_dataframes'
-fits_path = './data/cannon_training_data'
+df_path = './data/cannon_training_data'
 
 # function to load known tables with known binaries
 k16_path = './data/literature_data/Kraus2016/'
@@ -26,6 +27,16 @@ def load_kraus_table(filename):
 # clip size to clip 5% each end of order (in pixels)
 # used to generate training set
 order_clip = 200
+
+# write clipped wavelength data to reference file
+original_w_filename = './data/cks-spectra/cks-k00002_rj122.92.fits' # can be any r chip file
+w_data = fits.open(original_w_filename)[2].data[:, :-1] # require even number of elements
+w_data = w_data[:, order_clip:-1*order_clip] # clip 5% on each side
+reference_w_filename = './data/cannon_training_data/cannon_reference_w.fits'
+fits.HDUList([fits.PrimaryHDU(w_data)]).writeto(reference_w_filename, overwrite=True)
+print('clipped reference wavlength saved to {}'.format(reference_w_filename))
+
+# ============ clean training set + write labels to file  =========================================
 
 # load table with CKS properties from CKS website
 cks_stars = pd.read_csv(
@@ -70,7 +81,7 @@ kraus_binaries = pd.concat([
     load_kraus_table('Kraus2016_Table6.csv')]) # multi-PSF fitting
 # query targets where any separation within reported uncertainties
 # falls within hires slit width of 0.8arcsec
-hires_slit_width = 800 #0.8arcsec = 800mas
+hires_slit_width = 800 # 0.8arcsec = 800mas
 kraus_binaries = kraus_binaries.query('sep_mas - sep_err < @hires_slit_width')
 # add column with starnames to match training set table
 kraus_binaries['id_starname'] = kraus_binaries['KOI'].str.replace('KOI-', 'K0')
@@ -82,8 +93,10 @@ print(len(cks_stars), ' after removing unresolved binaries from Kraus 2016')
 cks_stars.to_csv('./data/label_dataframes/training_labels.csv')
 print('training labels saved to .csv file')
 
+# ============ write training flux, sigma to files  ================================================
+
 # re-write code below to save data for particular order
-def write_training_set_to_file(order_idx):
+def single_order_training_data(order_idx):
 
     # order numbers are not zero-indexed
     order_n = order_idx + 1
@@ -138,40 +151,35 @@ def write_training_set_to_file(order_idx):
         sigma_list.append(sigma_norm)
         id_starname_list.append(row.id_starname)
 
-    flux_df = pd.DataFrame(dict(zip(id_starname_list, flux_list)))
-    sigma_df = pd.DataFrame(dict(zip(id_starname_list, sigma_list)))
+    # store flux, sigma data
+    flux_df_n = pd.DataFrame(dict(zip(id_starname_list, flux_list)))
+    sigma_df_n = pd.DataFrame(dict(zip(id_starname_list, sigma_list)))
 
-    # write training data to .csv files
-    flux_df.to_csv('{}/training_flux_order{}.csv'.format(df_path, order_n))
-    sigma_df.to_csv('{}/training_sigma_order{}.csv'.format(df_path, order_n))
-    print('training flux and sigma saved to .csv files')
+    # store order number
+    flux_df_n.insert(0, 'order_number', order_n)
+    sigma_df_n.insert(0, 'order_number', order_n)
 
-    # write training data to fits files
-    flux_filename = '{}/training_flux_order{}.fits'.format(fits_path, order_n)
-    sigma_filename = '{}/training_sigma_order{}.fits'.format(fits_path, order_n)
+    return flux_df_n, sigma_df_n
 
-    flux_arr = flux_df.to_numpy().T
-    sigma_arr = sigma_df.to_numpy().T
-
-    fits.HDUList([fits.PrimaryHDU(flux_arr)]).writeto(flux_filename, overwrite=True)
-    print('training flux array saved to {}'.format(flux_filename))
-
-    fits.HDUList([fits.PrimaryHDU(sigma_arr)]).writeto(sigma_filename, overwrite=True)
-    print('training sigma array saved to {}'.format(sigma_filename))
-
-
-# write training set data to files for all 16 orders
+# write training set flux, sigma for all orders to single file
+flux_df = pd.DataFrame()
+sigma_df = pd.DataFrame()
 for order_idx in range(0, 16):
-    write_training_set_to_file(order_idx)
+    flux_df_n, sigma_df_n = single_order_training_data(order_idx)
+    flux_df = pd.concat([flux_df, flux_df_n])
+    sigma_df = pd.concat([sigma_df, sigma_df_n])
+flux_df.to_csv('{}/training_flux.csv'.format(df_path), index=False)
+sigma_df.to_csv('{}/training_sigma.csv'.format(df_path), index=False)
+print('training flux and sigma saved to .csv files')
 
-# write clipped wavelength data to reference file
-original_w_filename = './data/cks-spectra/cks-k00002_rj122.92.fits' # can be any r chip file
-w_data = fits.open(original_w_filename)[2].data[:, :-1] # require even number of elements
-w_data = w_data[:, order_clip:-1*order_clip] # clip 5% on each side
-reference_w_filename = './data/cannon_training_data/cannon_reference_w.fits'
-fits.HDUList([fits.PrimaryHDU(w_data)]).writeto(reference_w_filename, overwrite=True)
-print('clipped reference wavlength saved to {}'.format(reference_w_filename))
-
+# okay, this will save them to dataframes 
+# I'll run this and then I want to generate the same one 
+# but without the wavelet decomposition
+# what should the variable be called?
+# should it be called wavelet_filtering=True?
+# let me see what that paper calls it
+# I think I'll call it filter_wavelets = True
+# then I'll run it again to generate both datasets.
 
 
 
