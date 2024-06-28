1bfff2ebbaa0d62ef9c56249c7793c2eb7b0d85c
Isabel Angelo <isabelangelo@astro.ucla.edu>
2024-06-28 16:42:38 -0700

    added code to save CKS/CKS-cool targets to rsync_hires_spectra.py

--- rsync_hires_spectra.py
+++ rsync_hires_spectra.py
@@ -1,18 +1,30 @@
+"""
+This code combines stars from CKS and CKS-cool, 
+saves their labels to a file 
+and transfers the spectra from cadence to the local machine
+"""
+from astropy.table import Table
+from astropy.io import fits
 import specmatchemp.library
 import pandas as pd
 import os
 import pexpect
 
 # table with CKS stars
-cks_stars = pd.read_csv(
-    './data/literature_data/cks_physical_merged_with_fileroots.csv',
-    dtype={'spectrum_fileroot': str}) # retains trailing zeros in spectrum fileroot
+cks_stars_path = '../cks_binaries/data/literature_data/Petigura2017_Table5.fits'
+cks_stars = Table(fits.open(cks_stars_path)[1].data).to_pandas()
+print(len(cks_stars), 'stars from CKS')
+
+# remove KOI-2864, which seems to have some RV pipeline processing errors
+cks_stars = cks_stars[~cks_stars.Name.str.contains('KOI-02864')]
+print(len(cks_stars), ' after removing stars with processing errors')
 
 # table with CKS-cool stars
 lib = specmatchemp.library.read_hdf()
 min_teff = 3100 # corresponding to q=0.2 for Sun-like star
-max_teff = cks_stars.cks_steff.min()
+max_teff = cks_stars['Teff'].min()
 cks_cool_stars = lib.library_params.query('@min_teff<Teff & Teff<@max_teff')
+print(len(cks_cool_stars), 'stars from CKS-cool')
 
 # store password for rsync
 password = "CPS<3RVs"
@@ -72,5 +84,42 @@ for fits_filename in ['rj351.570.fits', 'rj487.76.fits']:
         run_rsync(command)
         print('copied {} to ./data/kepler1656_spectra/'.format(object_name))
 
+# rename columns of CKS sample
+cks_cols_to_keep = ['Name', 'Obs','Teff', 'e_Teff', 'logg', 'e_logg', \
+                    '[Fe/H]', 'e_[Fe/H]','vsini', 'e_vsini']
+cks_stars = cks_stars[cks_cols_to_keep].rename(
+    columns={
+    "Name": "id_starname", 
+    "Obs": "obs_id",
+    "Teff": "teff",
+    "e_Teff": "teff_err", 
+    "e_logg": "logg_err", 
+    "[Fe/H]": "feh",
+    "e_[Fe/H]": "feh_err",
+    "e_vsini": "vsini_err"})
+cks_stars['sample'] = ['cks'] * len(cks_stars)
 
+# rename columns of CKS-cool
+cks_cool_cols_to_keep = ['cps_name', 'lib_obs','Teff', 'u_Teff', 'logg', 'u_logg', \
+                    'feh', 'u_feh','vsini']
+cks_cool_stars = cks_cool_stars[cks_cool_cols_to_keep].rename(
+    columns={
+    "cps_name": "id_starname", 
+    "lib_obs": "obs_id",
+    "Teff": "teff",
+    "u_Teff": "teff_err", 
+    "u_logg": "logg_err",
+    "u_feh": "feh_err"})
+cks_cool_stars['sample'] = ['cks-cool'] * len(cks_cool_stars)
 
+# combine samples for training set
+hires_stars = pd.concat([cks_stars, cks_cool_stars], ignore_index=True)
+
+# set nan vsini for cool stars to 2±2 km/s
+hires_stars = hires_stars.fillna(
+    value={"vsini": 2, "vsini_err": 2})
+
+# save to file
+hires_stars_filename = './data/label_dataframes/hires_stars.csv'
+hires_stars.to_csv(hires_stars_filename)
+print('table with CKS + CKS-cool stars saved to {}'.format(hires_stars_filename))
